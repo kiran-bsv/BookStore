5fa4c41c7064f00bec4aaa7b8f2583b06cc53404
kiran bsv <89276963+kiran-bsv@users.noreply.github.com>
2023-11-18 19:27:53 +0530

    Update EditBook.jsx

--- frontend/src/pages/EditBook.jsx
+++ frontend/src/pages/EditBook.jsx
@@ -37,7 +37,7 @@ const EditBook = () => {
     };
     setLoading(true);
     axios
-      .put(`http://localhost:5555/books/${id}`, data)
+      .put(`https://bookstore-api-pitm.onrender.com/books/${id}`, data)
       .then(() => {
         setLoading(false);
         enqueueSnackbar('Book Edited successfully', { variant: 'success' });
